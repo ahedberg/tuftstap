11a86e8074587b7ca522f8ded86d2ab87ea3ff65
Ashley Hedberg <ashley.hedberg@tufts.edu>
2014-08-26 10:25:09 -0400

    board and alumni data

---
+++ js/fixtures.js
@@ -0,0 +1,298 @@
+App.Members = [
+	{
+		firstName: "Rachelle",
+		lastName: "Alfred",
+		year: 2015,
+		school: "Arts & Sciences",
+		imageUrl: "static/members/alfred.jpg",
+		major: "Clinical Psychology",
+		secondMajor: "Child Development",
+		minor: ""
+	},
+	{
+		firstName: "Alexandra",
+		lastName: "Brumberg",
+		year: 2016,
+		school: "Arts & Sciences",
+		imageUrl: "static/members/brumberg.jpg",
+		major: "Chemistry",
+		secondMajor: "Mathematics",
+		minor: ""
+	},
+	{
+		firstName: "Nicole",
+		lastName: "Goodman",
+		year: 2017,
+		school: "Arts & Sciences",
+		imageUrl: "static/members/goodman.jpg",
+		major: "Undecided",
+		secondMajor: "",
+		minor: ""
+	},
+	{
+		firstName: "Ashley",
+		lastName: "Hedberg",
+		year: 2015,
+		school: "Engineering",
+		imageUrl: "static/members/hedberg.jpg",
+		major: "Computer Science",
+		secondMajor: "Applied Mathematics",
+		minor: ""
+	},
+	{
+		firstName: "Reena",
+		lastName: "Karasin",
+		year: 2017,
+		school: "Arts & Sciences",
+		imageUrl: "static/members/karasin.jpg",
+		major: "English",
+		secondMajor: "",
+		minor: ""
+	},
+	{
+		firstName: "Caroline",
+		lastName: "Higley",
+		year: 2016,
+		school: "Arts & Sciences",
+		imageUrl: "static/members/higley.jpg",
+		major: "Environmental Studies",
+		secondMajor: "Psychology",
+		minor: ""
+	},
+	{
+		firstName: "Hannah",
+		lastName: "Loss",
+		year: 2017,
+		school: "Arts & Sciences",
+		imageUrl: "static/members/loss.jpg",
+		major: "Undecided",
+		secondMajor: "",
+		minor: ""
+	},
+	{
+		firstName: "Annie",
+		lastName: "Nesbitt",
+		year: 2017,
+		school: "Arts & Sciences",
+		imageUrl: "static/members/nesbitt.jpg",
+		major: "Undecided",
+		secondMajor: "",
+		minor: ""
+	},
+	{
+		firstName: "Sara",
+		lastName: "Pantel",
+		year: 2015,
+		school: "Arts & Sciences",
+		imageUrl: "static/members/pantel.jpg",
+		major: "Spanish",
+		secondMajor: "Biology",
+		minor: ""
+	},
+	{
+		firstName: "Abby",
+		lastName: "Ryland",
+		year: 2016,
+		school: "Arts & Sciences",
+		imageUrl: "static/members/ryland.jpg",
+		major: "Biopsychology",
+		secondMajor: "",
+		minor: ""
+	},
+	{
+		firstName: "Michela",
+		lastName: "Schena",
+		year: 2015,
+		school: "Arts & Sciences",
+		imageUrl: "static/members/schena.jpg",
+		major: "International Relations",
+		secondMajor: "Economics",
+		minor: ""
+	},
+	{
+		firstName: "Miranda",
+		lastName: "Seeley",
+		year: 2016,
+		school: "Engineering",
+		imageUrl: "static/members/seeley.jpg",
+		major: "Civil Engineering",
+		secondMajor: "",
+		minor: ""
+	},
+	{
+		firstName: "Melissa",
+		lastName: "Watton",
+		year: 2016,
+		school: "Arts & Sciences",
+		imageUrl: "static/members/watton.jpg",
+		major: "Undecided",
+		secondMajor: "",
+		minor: ""
+	},
+	{
+		firstName: "Olivia",
+		lastName: "Wyatt",
+		year: 2017,
+		school: "Arts & Sciences",
+		imageUrl: "static/members/wyatt.jpg",
+		major: "Psychology",
+		secondMajor: "",
+		minor: ""
+	},
+	{
+		firstName: "Jillian",
+		lastName: "Turner",
+		year: 2015,
+		school: "Arts & Sciences",
+		imageUrl: "static/members/turner.jpg",
+		major: "Mathematics",
+		secondMajor: "",
+		minor: ""
+	},
+];
+
+App.Alumni = [
+  	{
+		firstName: "Clara",
+		lastName: "Bieck",
+		year: 2014,
+		school: "Arts & Sciences",
+		imageUrl: "static/alumni/bieck.jpg",
+		major: "Biology",
+		secondMajor: "",
+		minor: "",
+		sinceTufts: ""
+	},
+  	{
+		firstName: "Kaitlin",
+		lastName: "Deveau",
+		year: 2014,
+		school: "Arts & Sciences",
+		imageUrl: "static/alumni/deveau.jpg",
+		major: "Spanish",
+		secondMajor: "",
+		minor: "",
+		sinceTufts: ""
+	},
+  	{
+		firstName: "Mollie",
+		lastName: "Elkin",
+		year: 2014,
+		school: "Arts & Sciences",
+		imageUrl: "static/alumni/elkin.jpg",
+		major: "Child Development",
+		secondMajor: "",
+		minor: "Architectural Studies",
+		sinceTufts: "Mollie is now a graduate student at Tufts!"
+	},
+  	{
+		firstName: "Eloise",
+		lastName: "Libre",
+		year: 2014,
+		school: "Arts & Sciences",
+		imageUrl: "static/alumni/libre.jpg",
+		major: "History",
+		secondMajor: "",
+		minor: "Music",
+		sinceTufts: ""
+	},
+  	{
+		firstName: "Amanda",
+		lastName: "Bell",
+		year: 2012,
+		school: "Arts & Sciences",
+		imageUrl: "static/alumni/bell.jpg",
+		major: "Computer Science",
+		secondMajor: "",
+		minor: "Mathematics",
+		sinceTufts: "Amanda interned at Modkit in Cambridge during the summer of 2012. In the fall of 2012, she was working on a project at the Center for Engineering Education and Outreach at Tufts. She is now in graduate school at Vanderbilt."
+	},
+  	{
+		firstName: "Dana",
+		lastName: "Burton",
+		year: 2012,
+		school: "Arts & Sciences",
+		imageUrl: "static/alumni/burton.jpg",
+		major: "Political Science",
+		secondMajor: "Spanish",
+		minor: "",
+		sinceTufts: "Dana has moved to D.C. and begun the process of becoming a real person. She plans to get a job in the international development field--and, of course, to keep dancing!"
+	},
+  	{
+		firstName: "Arielle",
+		lastName: "Carpenter",
+		year: 2010,
+		school: "Arts & Sciences",
+		imageUrl: "static/alumni/carpenter.jpg",
+		major: "Psychology",
+		secondMajor: "Community Health",
+		minor: "",
+		sinceTufts: "Arielle has completed the five-year BA/MPH program at Tufts University School of Medicine in downtown Boston. She then went on to complete an intensive ten-month MBA degree at College des Ingenieurs in Paris, France. She also worked in Paris as a consultant at the headquarters of Sanofi, a leading pharmaceutical company, where she planned programs to diversify the top 250 leaders of the global organization. Starting in September 2012, she has a one-year contract to remain in Paris at Sanofi as project manager of their new employee wellness program, BWel!"
+	},
+  	{
+		firstName: "Alyssa",
+		lastName: "Ettinger",
+		year: 2012,
+		school: "Arts & Sciences",
+		imageUrl: "static/alumni/ettinger.jpg",
+		major: "Child Development",
+		secondMajor: "",
+		minor: "",
+		sinceTufts: "After graduation, Alyssa started work as a medical assistant at an opthamology clinic in Medford. In fall 2012, she returned to Tufts to take organic chemistry, the last and most daunting of her pre-med requirements, before beginning the application process for medical school. She plans to take advantage of her extra time at Tufts by sneaking back into Tap Ensemble practices whenever possible!"
+	},
+  	{
+		firstName: "Alexandra",
+		lastName: "Forin",
+		year: 2011,
+		school: "Arts & Sciences",
+		imageUrl: "static/alumni/forin.jpg",
+		major: "Architectural Studies",
+		secondMajor: "Italian Studies",
+		minor: "",
+		sinceTufts: "In the summer of 2012, Alexandra began her Master of Architecture degree at the University of Oregon, where she plans to focus on sustainable design. So far, it has been an exhausting but extremely rewarding experience. Of course, she misses all the wonderful ladies (and one guy) of Tap Ensemble! She has also just learned how to ride a bike and will soon be adopting a cat."
+	},
+  	{
+		firstName: "Marissa",
+		lastName: "Fruchter",
+		year: 2012,
+		school: "Arts & Sciences",
+		imageUrl: "static/alumni/fruchter.jpg",
+		major: "Clinical Psychology",
+		secondMajor: "",
+		minor: "Studio Art",
+		sinceTufts: "In August 2012, Marissa moved to Pennsylvania and started the PsyD program at Widener University, which is a five-year doctoral program in clinical psychology. She misses Tap Ensemble!"
+	},
+  	{
+		firstName: "Stephanie",
+		lastName: "Kerr",
+		year: 2014,
+		school: "Arts & Sciences",
+		imageUrl: "static/alumni/kerr.jpg",
+		major: "Psychology",
+		secondMajor: "",
+		minor: "",
+		sinceTufts: ""
+	},
+  	{
+		firstName: "Grace",
+		lastName: "Oberhofer",
+		year: 2015,
+		school: "Arts & Sciences",
+		imageUrl: "static/alumni/oberhofer.jpg",
+		major: "Music",
+		secondMajor: "",
+		minor: "",
+		sinceTufts: ""
+	},
+	{
+		firstName: "Evy",
+		lastName: "Roy",
+		year: 2016,
+		school: "Arts & Sciences",
+		imageUrl: "static/alumni/roy.jpg",
+		major: "Women, Gender, and Sexuality Studies",
+		secondMajor: "",
+		minor: "",
+		sinceTufts: ""
+	},
+];
